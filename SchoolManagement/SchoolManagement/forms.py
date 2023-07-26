b3ba4fba49d4558df4ba37d320c45d336f5a1ffc
Hemang Patel <hemang2719@gmail.com>
2023-07-26 20:11:27 +0530

    Register form changes

--- SchoolManagement/SchoolManagement/forms.py
+++ SchoolManagement/SchoolManagement/forms.py
@@ -11,6 +11,24 @@ class UserRegistrationForm(BaseUserCreationForm):
     class Meta:
         model=Users
         fields=['email','phone','first_name','last_name','standard','section','gender','DOB','state','city','address','department','role','password1','password2']
+        # css changes
+        widgets = {
+            'email': forms.EmailInput(attrs={ 'class' : 'form-control'}),
+            'first_name': forms.TextInput(attrs={ 'class' : 'form-control 1'}),
+            'password1': forms.TextInput(attrs={ 'class' : 'form-control'}),
+            'password2': forms.TextInput(attrs={ 'class' : 'form-control'}),
+            'last_name': forms.TextInput(attrs={ 'class' : 'form-control'}),
+            'DOB': forms.DateInput(attrs={ 'class' : 'form-control'}),
+            'phone': forms.TextInput(attrs={'class' : 'form-control'}),
+            'standard': forms.Select(attrs={'class' : 'form-control'}),
+            'section': forms.Select(attrs={'class' : 'form-control'}),
+            'gender': forms.Select(attrs={'class' : 'form-control'}),
+            'state': forms.Select(attrs={'class' : 'form-control'}),
+            'city': forms.Select(attrs={'class' : 'form-control'}),
+            'department': forms.Select(attrs={'class' : 'form-control'}),
+            'role': forms.Select(attrs={'class' : 'form-control'}),
+            'address': forms.Textarea(attrs={'class' : 'form-control'}),
+        }
 
 
 
